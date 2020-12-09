44d57e390d1968b4e3baebf61543b0db29f17aba
Nikita Vanychenko <postamentbox@bk.ru>
2020-12-09 14:34:42 +0300

    Work on ten screen

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import First_screen from './components/First-screen.jsx';
 import Second_screen from './components/Second-screen.jsx';
 import Third_screen from './components/Third-screen.jsx';
 import Nine_screen from './components/Nine-screen.jsx';
+import Ten_screen from './components/Ten-screen.jsx';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Second_screen />
         <Third_screen />
         <Nine_screen />
+        <Ten_screen />
 
     </div>
   );
