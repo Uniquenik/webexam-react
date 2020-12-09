fb6649fd70e01ebb759ebe546f5f20abd89b02b1
Nikita Vanychenko <postamentbox@bk.ru>
2020-12-09 14:44:52 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -5,17 +5,9 @@ import Navmobile from './components/Navmobile.jsx';
 import First_screen from './components/First-screen.jsx';
 import Second_screen from './components/Second-screen.jsx';
 import Third_screen from './components/Third-screen.jsx';
-<<<<<<< HEAD
 import Fourth_screen from './components/Fourth-screen.jsx';
-
-=======
 import Nine_screen from './components/Nine-screen.jsx';
-<<<<<<< HEAD
 import Ten_screen from './components/Ten-screen.jsx';
-=======
->>>>>>> 937ed00fc6270f665f25e66c24454be38c9ddba3
->>>>>>> d29a8a77fb44131869ca652ad3e047efc12ead44
-
 
 function App() {
   return (
@@ -24,13 +16,10 @@ function App() {
         <First_screen />
         <Second_screen />
         <Third_screen />
-<<<<<<< HEAD
         <Fourth_screen />
-=======
         <Nine_screen />
         <Ten_screen />
 
->>>>>>> 937ed00fc6270f665f25e66c24454be38c9ddba3
     </div>
   );
 }
